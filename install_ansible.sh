1e236fd12660d32749351c6436741be4b9d7f546
Vinay Sanjivaiah <60195382+vsanjivaiah@users.noreply.github.com>
2020-12-09 07:05:00 +0530

    updated bash script

--- install_ansible.sh
+++ install_ansible.sh
@@ -8,24 +8,31 @@ LOGFILE="/var/log/ansible.log"
 # Add epel repo and packages
 add_packages_func() {
   sudo dnf makecache
-  sudo dnf update && sudo dnf upgrade -y
-  sudo dnf install epel-release -y
-  sudo dnf install -y \
+  sudo yum install -y epel-release
+  sudo yum install -y \
     git \
     ansible
+    # mkdir /opt/{terraform_0.13.5,packer_1.6.4}
+    # cd /opt/terraform_0.13.5 && curl -O https://releases.hashicorp.com/terraform/0.13.5/terraform_0.13.5_linux_amd64.zip
+    # cd /opt/packer_1.6.4 && curl -O https://releases.hashicorp.com/packer/1.6.4/packer_1.6.4_linux_amd64.zip
+    # cd /opt/terraform_0.13.5 && unzip terraform_0.13.5_linux_amd64.zip && sudo ln -s ~/terraform_0.13.5/terraform /usr/bin/terraform
+    # cd /opt/packer_1.6.4 && unzip packer_1.6.4_linux_amd64.zip && sudo ln -s ~/packer_1.6.4/packer /usr/bin/packer
+    sudo pip3 install -r https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt  
+    sudo ansible-galaxy collection install azure.azcollection --force
+    sudo pip3 install "pywinrm>=0.3.0"
 }
 
 # Create ansible dir and clone github repo in newly created dir
 clone_ansible_repo_func() {
+  sudo curl  https://e360artifacts.blob.core.windows.net/artifacts/citrixxd-ansible.zip --output /root/citrixxd-ansible.zip
   sudo mkdir /opt/ansible
-  sudo chown -R :adm /opt/ansible/
-  sudo git clone https://github.com/entisys360/Desktop360-mgsv-ansible.git /opt/ansible
-  sudo chown -R :adm /opt/ansible/
+  sudo unzip /root/citrixxd-ansible.zip -d /opt/ansible/
+  sudo rm -f /root/citrixxd-ansible.zip
 }
 
 # Execute Ansible plays
 ansible_execute_func() {
-  (cd /opt/ansible/ && ansible-playbook -vv /opt/ansible/setup.yml)
+  ansible-playbook /opt/ansible/setup.yml
 }
 
 # Echo messages to log file
@@ -36,26 +43,10 @@ log_func() {
 # Execute functions
 main() {
   add_packages_func
-  add_epel_repo_func
-    if [ "$?" == 0 ]; then 
-      log_func "[INFO]: epel-release and selected packages successfully installed."
-    else
-      log_func "[ERROR]: $?"
-    fi
-
   clone_ansible_repo_func
-  if [ "$?" == 0 ]; then 
-    log_func "[INFO]: Git repository successfully cloned to /opt/ansible"
-  else
-    log_func "[ERROR]: $?"
-  fi
-
-  ansible_execute_func
-  touch /tmp/machine_catalog.json
-  touch /tmp/hosting_connection.json
-  echo "$1" > /tmp/machine_catalog.json
-  echo "$2" > /tmp/hosting_connection.json
+  sudo echo "$1" > /opt/ansible/citrixxd-ansible/mc.json
+  sudo echo "$2" > /opt/ansible/citrixxd-ansible/hc.json
+  sudo echo "$3" > /opt/ansible/citrixxd-ansible/dg.json
 }
 
 main "$@"
-
